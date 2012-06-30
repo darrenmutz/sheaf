6cb6b286412ae51c69754139f492f9e31c212c91
Darren Mutz <darren@mutz.org>
2012-06-30 08:58:59 -0700

    Upgrade to clojure 1.4. Clean up project name.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject sheaf "0.0.1-SNAPSHOT"
+(defproject sheaf "0.0.1"
   :description "Static blogging engine"
   :url "https://github.com/darrenmutz/sheaf"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.lucene/lucene-core "3.0.2"]
                  [org.clojure/tools.cli "0.2.1"]
                  [lancet "1.0.0"]
