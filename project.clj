860d9db0c3ca45ac9f03a61e38e3238d4eaf11d7
Darren Mutz <darren@mutz.org>
2013-06-11 09:33:02 -0700

    Upgrade to Clojure 1.5.1.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [lancet "1.0.0"]
                  [org.apache.lucene/lucene-core "3.0.2"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojure/tools.cli "0.2.1"]]
   :dev-dependencies [[lein-marginalia "0.7.1"]]
